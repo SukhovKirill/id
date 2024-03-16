8055e0f4d9fc002617b4c50bcc62b669dd6206ed
SukhovKirill <160316848+SukhovKirill@users.noreply.github.com>
2024-03-16 11:46:22 +0300

    Update mydb.sql

--- mydb.sql
+++ mydb.sql
@@ -66,3 +66,46 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `ip_addresses`
+--
+
+CREATE TABLE `ip_addresses` (
+  `id` int NOT NULL,
+  `ip_address` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `is_allowed` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `ip_addresses`
+--
+
+INSERT INTO `ip_addresses` (`id`, `ip_address`, `is_allowed`) VALUES
+(1, '127.0.0.1', 1);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `ip_addresses`
+--
+ALTER TABLE `ip_addresses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `ip_addresses`
+--
+ALTER TABLE `ip_addresses`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
